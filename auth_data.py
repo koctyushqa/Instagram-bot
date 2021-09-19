cb481c37202fe5d42cbfada0306160b6397fddde
Konstantin <kovucholegich@mail.ru>
2021-09-19 18:45:53 +0300

    Part7_fixed_bug_in_lines_340_355_and_switching_between_bot_accounts

--- auth_data.py
+++ auth_data.py
@@ -1,2 +1,21 @@
 username = "Your name"
 password = "Your password"
+
+# Variable with a list of users for mass mailing
+# (Переменная со списком пользователей для массовой рассылки)
+direct_users_list = ['user1', 'user2', 'user3', "..."]
+
+# Dictionary for accounts: key - username, value - nested dictionaries, with login and password values
+# (Словарь для аккаунтов: ключ - username, значение - вложенные словари, со значениями логин и пароль)
+users_settings_dict = {
+    'user1': {
+        'login': 'bot_login',
+        'password': 'bot_password',
+        'window_size': '800, 600'
+    },
+    'user2': {
+        'login': 'bot_login',
+        'password': 'bot_password',
+        'window_size': '1600, 900'
+    }
+}
